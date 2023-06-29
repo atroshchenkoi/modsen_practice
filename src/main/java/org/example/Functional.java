5fc3ee40b9f05879da5792608dd56b140323d65e
atros <iatroshchnka@gmail.com>
2023-06-29 15:54:09 +0300

    2

---
+++ src/main/java/org/example/Functional.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.util.regex.Matcher;
+
+@FunctionalInterface
+public interface Functional {
+    public String calculate(Matcher currentMatcher);
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,50 +5,58 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    final static Pattern PATTERN_TO_DOLLARS = Pattern.compile("toDollars\\((([0-9]*\\.?[0-9]+)\\р)\\)");
-    final static Pattern PATTERN_TO_RUBLES = Pattern.compile("toRubles\\((\\$([0-9]*\\.?[0-9]+))\\)");
-    final static Pattern PATTERN_OPERATION_WITH_DOLLARS = Pattern.compile("\\$([0-9]*\\.?[0-9]+)\\s([+-])\\s\\$([0-9]*\\.?[0-9]+)");
-    final static Pattern PATTERN_OPERATION_WITH_RUBLES = Pattern.compile("([0-9]*\\.?[0-9]+)р\\s([+-])\\s([0-9]*\\.?[0-9]+)р");
-    final static Pattern PATTERN_RESULTING_STRING = Pattern.compile("^\\$?([0-9]*\\.?[0-9]+)р?$");
+    final static Pattern PATTERN_TO_DOLLARS = Pattern.compile("toDollars\\(((\\-?[0-9]*\\.?[0-9]+)\\р)\\)");
+    final static Pattern PATTERN_TO_RUBLES = Pattern.compile("toRubles\\((\\$(\\-?[0-9]*\\.?[0-9]+))\\)");
+    final static Pattern PATTERN_OPERATION_WITH_DOLLARS = Pattern.compile("\\$(\\-?[0-9]*\\.?[0-9]+)\\s([+-])\\s\\$(\\-?[0-9]*\\.?[0-9]+)");
+    final static Pattern PATTERN_OPERATION_WITH_RUBLES = Pattern.compile("(\\-?[0-9]*\\.?[0-9]+)р\\s([+-])\\s(\\-?[0-9]*\\.?[0-9]+)р");
+    final static Pattern PATTERN_RESULTING_STRING = Pattern.compile("^\\$?(\\-?[0-9]*\\.?[0-9]+)р?$");
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder stringBuilder = new StringBuilder(sc.nextLine());
-        Matcher currentlyMatcher;
+        Matcher currentMatcher;
+        boolean flag = false;
         while(!stringBuilder.toString().matches(PATTERN_RESULTING_STRING.pattern())){
-            if ((currentlyMatcher = PATTERN_TO_DOLLARS.matcher(stringBuilder)).find()) {
-                StringBuilder sb = new StringBuilder();
-                currentlyMatcher.appendReplacement(sb, Calculator.toDollars(Double.parseDouble(currentlyMatcher.group(2))));
-                currentlyMatcher.appendTail(sb);
-                stringBuilder = new StringBuilder(sb.toString());
+            if ((currentMatcher = PATTERN_TO_DOLLARS.matcher(stringBuilder)).find()) {
+                stringBuilder = new StringBuilder(expressionHelping(currentMatcher, currentMatcherExpression ->
+                        Calculator.toDollars(Double.parseDouble(currentMatcherExpression.group(2)))));
+                flag = true;
             }
-            if ((currentlyMatcher = PATTERN_TO_RUBLES.matcher(stringBuilder)).find()) {
-                StringBuilder sb = new StringBuilder();
-                currentlyMatcher.appendReplacement(sb, Calculator.toRubles(Double.parseDouble(currentlyMatcher.group(2))));
-                currentlyMatcher.appendTail(sb);
-                stringBuilder = new StringBuilder(sb.toString());
+            if ((currentMatcher = PATTERN_TO_RUBLES.matcher(stringBuilder)).find()) {
+                stringBuilder = new StringBuilder(expressionHelping(currentMatcher, currentMatcherExpression ->
+                        Calculator.toRubles(Double.parseDouble(currentMatcherExpression.group(2)))));
+                flag = true;
             }
-            if ((currentlyMatcher = PATTERN_OPERATION_WITH_DOLLARS.matcher(stringBuilder)).find()) {
-                StringBuilder sb = new StringBuilder();
-                currentlyMatcher.appendReplacement(sb,"$" + Calculator.operation(
-                        Double.parseDouble(currentlyMatcher.group(1)),
-                        Double.parseDouble(currentlyMatcher.group(3)),
-                        currentlyMatcher.group(2)));
-                currentlyMatcher.appendTail(sb);
-                stringBuilder = new StringBuilder(sb.toString());
+            if ((currentMatcher = PATTERN_OPERATION_WITH_DOLLARS.matcher(stringBuilder)).find()) {
+                stringBuilder = new StringBuilder(expressionHelping(currentMatcher, currentMatcherExpression ->
+                        "$" + Calculator.operation(
+                                Double.parseDouble(currentMatcherExpression.group(1)),
+                                Double.parseDouble(currentMatcherExpression.group(3)),
+                                currentMatcherExpression.group(2))));
+                flag = true;
             }
-            if ((currentlyMatcher = PATTERN_OPERATION_WITH_RUBLES.matcher(stringBuilder)).find()) {
-                StringBuilder sb = new StringBuilder();
-                currentlyMatcher.appendReplacement(sb, Calculator.operation(
-                        Double.parseDouble(currentlyMatcher.group(1)),
-                        Double.parseDouble(currentlyMatcher.group(3)),
-                        currentlyMatcher.group(2)) + 'р');
-                currentlyMatcher.appendTail(sb);
-                stringBuilder = new StringBuilder(sb.toString());
+            if ((currentMatcher = PATTERN_OPERATION_WITH_RUBLES.matcher(stringBuilder)).find()) {
+                stringBuilder = new StringBuilder(expressionHelping(currentMatcher, currentMatcherExpression ->
+                        Calculator.operation(
+                                Double.parseDouble(currentMatcherExpression.group(1)),
+                                Double.parseDouble(currentMatcherExpression.group(3)),
+                                currentMatcherExpression.group(2)) + 'р'));
             }
+            if(!flag) {
+                throw new RuntimeException(stringBuilder + "\n <- the reduction cannot be performed...check the expression.");
+            }
+            flag = false;
+
         }
         char ch = stringBuilder.charAt(0) == '$' ? '$' : 'р';
         double res = Calculator.round(Double.parseDouble(stringBuilder.toString().replaceAll("\\$?\\р?", "")));
         System.out.println(ch == '$' ? String.valueOf(ch) + res : res + String.valueOf(ch));
+    }
+
+    public static StringBuilder expressionHelping(Matcher currentMatcher, Functional functional) {
+        StringBuilder sb = new StringBuilder();
+        currentMatcher.appendReplacement(sb, functional.calculate(currentMatcher));
+        currentMatcher.appendTail(sb);
+        return sb;
     }
 }
